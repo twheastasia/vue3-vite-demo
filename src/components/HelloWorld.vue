d0337dae414d46605563c17f09fbc7c2dcec4748
weihai.tang <weihai.tang@alo7.com>
2020-09-30 14:44:13 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 @contextmenu="stopContextMenu">{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    stopContextMenu(event) {
+      event.preventDefault()
+    }
+  }
+}
+</script>
